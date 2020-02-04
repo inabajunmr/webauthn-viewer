b8511c634d40901600880e11c95fcdb53cbd6d73
inabajunmr <inabajun.for.regi@gmail.com>
2020-02-04 19:35:32 +0900

    wip

---
+++ client/src/components/Get.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Lorem <br />
+    ipsum <br />
+    dolor <br />
+    sit <br />
+    amet.
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ client/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-tabs>
+      <b-tab-item label="navigator.credentias.create()">
+        <Create />
+      </b-tab-item>
+
+      <b-tab-item label="navigator.credentias.get()">
+        <Get />
+      </b-tab-item>
+    </b-tabs>
+  </div>
+</template>
+
+<script></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
