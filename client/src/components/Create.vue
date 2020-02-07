e5a01ea99d659a80c02c59859b681d1748d11f73
inabajunmr <inabajun.for.regi@gmail.com>
2020-02-07 20:57:16 +0900

    wip

--- client/src/components/Create.vue
+++ client/src/components/Create.vue
@@ -340,7 +340,7 @@
             <tr>
               <th style="padding-left: 20px">.clientDataJSON</th>
               <td style="word-wrap: break-word">
-                {{ createResponseResponseClientDataJSON }}
+                {{ createResponseView.clientDataJSON }}
               </td>
             </tr>
             <tr>
@@ -355,7 +355,7 @@
               <th style="padding-left: 60px">.rpidHash</th>
               <td style="word-wrap: break-word">
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.rpidHash
+                  createResponseView.rpidHash
                 }}
               </td>
             </tr>
@@ -363,11 +363,7 @@
               <th style="padding-left: 60px">.flag.up</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.flag ==
-                  undefined
-                    ? ""
-                    : createResponseResponseAttestationObjectAuthDataForView
-                        .flag.up
+                  createResponseView.up
                 }}
               </td>
             </tr>
@@ -375,11 +371,7 @@
               <th style="padding-left: 60px">.flag.uv</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.flag ==
-                  undefined
-                    ? ""
-                    : createResponseResponseAttestationObjectAuthDataForView
-                        .flag.uv
+                  createResponseView.uv
                 }}
               </td>
             </tr>
@@ -387,11 +379,7 @@
               <th style="padding-left: 60px">.flag.at</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.flag ==
-                  undefined
-                    ? ""
-                    : createResponseResponseAttestationObjectAuthDataForView
-                        .flag.at
+                  createResponseView.at
                 }}
               </td>
             </tr>
@@ -399,11 +387,7 @@
               <th style="padding-left: 60px">.flag.ed</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.flag ==
-                  undefined
-                    ? ""
-                    : createResponseResponseAttestationObjectAuthDataForView
-                        .flag.ed
+                  createResponseView.ed
                 }}
               </td>
             </tr>
@@ -411,7 +395,7 @@
               <th style="padding-left: 60px">.signCount</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.signCount
+                  createResponseView.signCount
                 }}
               </td>
             </tr>
@@ -419,7 +403,7 @@
               <th style="padding-left: 60px">.aaguid</th>
               <td>
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.aaguid
+                  createResponseView.aaguid
                 }}
               </td>
             </tr>
@@ -427,7 +411,7 @@
               <th style="padding-left: 60px">.credentialId</th>
               <td style="word-wrap: break-word">
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.credentialId
+                  createResponseView.credentialId
                 }}
               </td>
             </tr>
@@ -435,30 +419,30 @@
               <th style="padding-left: 60px">.credentialPublicKey</th>
               <td style="word-wrap: break-word">
                 {{
-                  createResponseResponseAttestationObjectAuthDataForView.credentialPublicKey
+                  createResponseView.credentialPublicKey
                 }}
               </td>
             </tr>
             <tr>
               <th style="padding-left: 40px">.fmt</th>
-              <td>{{ createResponseResponseAttestationObjectFmt }}</td>
+              <td>{{ createResponseView.fmt }}</td>
             </tr>
             <tr>
               <th style="padding-left: 40px">.attStmt</th>
               <td>
-                {{ createResponseResponseAttestationObjectAttStmt }}
+                {{ createResponseView.attStmt }}
               </td>
             </tr>
             <tr>
               <th>.id</th>
               <td style="word-wrap: break-word">
-                {{ createResponseId }}
+                {{ createResponseView.id }}
               </td>
             </tr>
             <tr>
               <th>.type</th>
               <td style="word-wrap: break-word">
-                {{ createResponseType }}
+                {{ createResponseView.type }}
               </td>
             </tr>
           </tbody>
@@ -488,15 +472,8 @@ export default {
       reqAttestation: "direct",
       reqTimeout: 60000,
       reqChallenge: this.generateChallenge(),
-      createResult: "",
-      resRawId: "",
-      resResponseAttestationObject: "",
-      resResponseClientDataJSON: "",
-      resId: "",
-      resType: "",
       reqExcludeCredentials: [],
       createResponse: {},
-      transports: ["usb", "ble", "nfc", "internal"]
     };
   },
   computed: {
@@ -557,101 +534,83 @@ export default {
       }
       return request;
     },
-    createResponseRawId: function() {
-      return new Int8Array(this.createResponse.rawId);
-    },
-    createResponseResponseAttestationObjectFmt: function() {
-      if (this.createResponse.response == undefined) {
-        return "";
-      }
-      var cbor = require("cbor");
-      return cbor.decodeAllSync(
-        new Buffer(this.createResponse.response.attestationObject)
-      )[0].fmt;
-    },
-    createResponseResponseAttestationObjectAttStmt: function() {
-      if (this.createResponse.response == undefined) {
-        return "";
-      }
-      var cbor = require("cbor");
-      return cbor.decodeAllSync(
-        new Buffer(this.createResponse.response.attestationObject)
-      )[0].attStmt;
-    },
-    createResponseResponseAttestationObjectAuthDataForView: function() {
-      if (this.createResponse.response == undefined) {
-        return "";
-      }
-      console.log(this.createResponse.response);
-      var cbor = require("cbor");
-      var authDataArray = cbor.decodeAllSync(
-        new Buffer(this.createResponse.response.attestationObject)
-      )[0].authData;
+    createResponseView: function() {
+      // refference https://medium.com/@herrjemand/verifying-fido2-responses-4691288c8770
+      let result = {}
+      result.id = this.createResponse.id
+      result.type = this.createResponse.type
+      if (this.createResponse.response){
+        /** clientDataJSON */
+        let enc = new TextDecoder("utf-8");
+        result.clientDataJSON = enc.decode(this.createResponse.response.clientDataJSON);
 
-      // parse authDataArray https://www.w3.org/TR/webauthn/#authenticator-data
-      const rpidHash = authDataArray.slice(0, 32);
-      const flag = authDataArray.slice(32, 33); //.readUInt8(0)
-      const signCount =
-        (authDataArray[33] << 24) |
-        (authDataArray[34] << 16) |
-        (authDataArray[35] << 8) |
-        authDataArray[36];
-        console.log(flag)
-        const up = Boolean(flag & 0x01)
-        const uv = Boolean(flag & 0x04)
-        const at = Boolean(flag & 0x64)
-        const ed = Boolean(flag & 0x128)
+        /** attestationObject */
+        const cbor      = require('cbor');
+        const vanillacbor   = require('vanillacbor');
 
-      let aaguid;
-      let credentialId;
-      let credentialPublicKey;
-      if (at) {
-        aaguid = Buffer.from(authDataArray.slice(37, 53)).toString("hex");
-        const credentialIdLengthTmp = authDataArray.slice(53, 55);
-        const credentialIdLength =
-          (credentialIdLengthTmp[0] << 8) + credentialIdLengthTmp[1];
-        credentialId = btoa(
-          String.fromCharCode(
-            ...authDataArray.slice(55, 55 + credentialIdLength)
-          )
-        );
-        credentialPublicKey = btoa(
-          String.fromCharCode(...authDataArray.slice(55 + credentialIdLength))
-        );
-      }
+        let attestationObject = cbor.decodeAllSync(
+          new Buffer(this.createResponse.response.attestationObject)
+        )[0];
+        result.fmt = attestationObject.fmt
+        result.attStmt = attestationObject.attStmt
 
-      // TODO Extension data
-      return {
-        rpidHash: Buffer.from(rpidHash).toString("hex"),
-        flag: {
-          up: up,
-          uv: uv,
-          at: at,
-          ed: ed
-        },
-        signCount: signCount,
-        aaguid: aaguid,
-        credentialId: credentialId,
-        credentialPublicKey: credentialPublicKey
-      };
-    },
-    createResponseResponseAttestationObjectExtension: function() {
-      return require("cbor").decodeAllSync(
-        new Buffer(this.createResponse.response.extensions)
-      )[0];
-    },
-    createResponseResponseClientDataJSON: function() {
-      if (this.createResponse.response == undefined) {
-        return "";
-      }
-      var enc = new TextDecoder("utf-8");
-      return enc.decode(this.createResponse.response.clientDataJSON);
-    },
-    createResponseId: function() {
-      return this.createResponse.id;
-    },
-    createResponseType: function() {
-      return this.createResponse.type;
+        let buffer = attestationObject.authData
+        let rpIdHash      = buffer.slice(0, 32);             buffer = buffer.slice(32);
+        result.authData = {}
+        result.rpIdHash = rpIdHash.toString('hex')
+
+        /* Flags */
+        let flagsBuffer   = buffer.slice(0, 1);              buffer = buffer.slice(1);
+        let flagsInt      = flagsBuffer[0];
+        result.up            = !!(flagsInt & 0x01); // Test of User Presence
+        result.uv            = !!(flagsInt & 0x04); // User Verification
+        result.at            = !!(flagsInt & 0x40); // Attestation data
+        result.ed            = !!(flagsInt & 0x80); // Extension data
+
+        let counterBuffer = buffer.slice(0, 4);               buffer = buffer.slice(4);
+        result.signCount       = counterBuffer.readUInt32BE(0);
+
+        /* Attested credential data */
+        let aaguid              = undefined;
+        let aaguidBuffer        = undefined;
+        let credIdBuffer        = undefined;
+        let cosePublicKeyBuffer = undefined;
+        let attestationMinLen   = 16 + 2 + 16 + 77; // aaguid + credIdLen + credId + pk
+
+        if(result.at) { // Attested Data
+            if(buffer.byteLength < attestationMinLen)
+                throw new Error(`It seems as the Attestation Data flag is set, but the remaining data is smaller than ${attestationMinLen} bytes. You might have set AT flag for the assertion response.`)
+
+            aaguid              = buffer.slice(0, 16).toString('hex'); buffer = buffer.slice(16);
+            aaguidBuffer        = `${aaguid.slice(0, 8)}-${aaguid.slice(8, 12)}-${aaguid.slice(12, 16)}-${aaguid.slice(16, 20)}-${aaguid.slice(20)}`;
+            result.aaguid = aaguidBuffer
+
+            let credIdLenBuffer = buffer.slice(0, 2);                  buffer = buffer.slice(2);
+            let credIdLen       = credIdLenBuffer.readUInt16BE(0);
+            credIdBuffer        = buffer.slice(0, credIdLen);          buffer = buffer.slice(credIdLen);
+            result.credentialId = credIdBuffer.toString('hex')
+
+            let pubKeyLength    = vanillacbor.decodeOnlyFirst(buffer).byteLength;
+            cosePublicKeyBuffer = buffer.slice(0, pubKeyLength);       buffer = buffer.slice(pubKeyLength);
+            result.credentialPublicKey = cosePublicKeyBuffer.toString('hex')
+        }
+
+        let coseExtensionsDataBuffer = undefined;
+        if(result.ed) { // Extension Data
+            let extensionsDataLength = vanillacbor.decodeOnlyFirst(buffer).byteLength;
+
+            coseExtensionsDataBuffer = buffer.slice(0, extensionsDataLength); buffer = buffer.slice(extensionsDataLength);
+            result.coseExtensionsDataBuffer = coseExtensionsDataBuffer.toString('hex')
+        }
+
+        if(buffer.byteLength)
+            throw new Error('Failed to decode authData! Leftover bytes been detected!');
+
+      }      
+
+      console.log(result)
+      return result
+
     }
   },
   methods: {
@@ -668,6 +627,7 @@ export default {
         .then(res => {
           console.log(res);
           this.createResponse = res;
+          this.createResponseView // TODO remove
         })
         .catch(err => {
           console.log(err);
---
+++ node_modules/vanillacbor/lib/vanillacbor.js
@@ -0,0 +1,355 @@
+/*
+ * vanilla CBOR decoder / encoder
+ * https://github.com/herrjemand/vanillaCBOR
+ *
+ * Copyright (c) 2018 Yuriy Ackermann <ackermann.yuriy@gmail.com>
+ * Licensed under the MIT license.
+ */
+(function(){
+    'use strict';
+
+    let tags = {
+        0: 'UNSIGNED_INT',
+        1: 'NEGATIVE_INT',
+        2: 'BYTE_STRING',
+        3: 'TEXT_STRING',
+        4: 'ARRAY',
+        5: 'MAP',
+        6: 'OTHER_SEM',
+        7: 'FLOAT_AND_NO_CONTENT'
+    }
+
+    let float_and_no_content_semantics = {
+        20: "FALSE",
+        21: "TRUE",
+        22: "NULL",
+        23: "UNDEFINED",
+        24: "SIMPLE_CONT", // Simple value (value 32..255 in following byte)
+        25: "FLOAT_16", // IEEE 754 Half-Precision Float (16 bits follow)
+        26: "FLOAT_32", // IEEE 754 Single-Precision Float (32 bits follow)
+        27: "FLOAT_64", // IEEE 754 Double-Precision Float (64 bits follow)
+        31: "INFINITY_BREAK"
+    }
+
+    var type = (obj) => {
+        return {}.toString.call(obj)
+                 .replace(/\[|\]/g, '')
+                 .split(' ')[1];
+    }
+
+    var enforceUint8Array = (buffer) => {
+        let bufferType = type(buffer)
+        if( bufferType != 'Uint8Array' &&
+            bufferType != 'Uint16Array' &&
+            bufferType != 'Uint32Array' &&
+            bufferType != 'ArrayBuffer' )
+           throw new TypeError('Only BufferSource is allowed!');
+
+        if(bufferType == 'ArrayBuffer')
+            return new Uint8Array(buffer.slice())
+        else
+            return new Uint8Array(buffer.buffer.slice())
+
+        return temp
+    }
+
+
+    let mergeTwoBuffers = function(buffer1, buffer2) {
+        buffer1 = enforceBigEndian(enforceUint8Array(buffer1));
+        buffer2 = enforceBigEndian(enforceUint8Array(buffer2));
+
+        let totalLength = buffer1.length + buffer2.length;
+
+        let mergedBuffers = new Uint8Array(totalLength);
+        mergedBuffers.set(buffer1);
+        mergedBuffers.set(buffer2, buffer1.length);
+
+        return mergedBuffers
+    }
+
+    let isEndianBig = () => {
+        let buff = new ArrayBuffer(2);
+        let u8   = new Uint8Array(buff);
+        let u16  = new Uint16Array(buff);
+        u8[0] = 0xCC;
+        u8[1] = 0xDD;
+
+        if(u16[0] !== 0xDDCC)
+            return false
+
+        return true
+    }
+
+    let enforceBigEndian = (buffer) => {
+        if(isEndianBig())
+            buffer = buffer.reverse();
+
+        return buffer
+    }
+
+    let readBE81632 = (buffer) => {
+        if(typeof Buffer === 'undefined')
+            buffer = enforceUint8Array(buffer);
+        
+        buffer = enforceBigEndian(buffer);
+
+        if(buffer.length !== 1 && buffer.length !== 2 && buffer.length !== 4)
+            throw new Error('Only 2/4/16 byte buffers are allowed!');
+
+        buffer = new Uint8Array(Array.from(buffer));
+        if(buffer.length === 1)
+            return buffer[0]
+        else if(buffer.length === 2)
+            return new Uint16Array(buffer.buffer)[0]
+        else
+            return new Uint32Array(buffer.buffer)[0]
+    }
+
+    let getTLVForNext = (buffer) => {
+        let lennum   = buffer[0] - (buffer[0] & 32) - (buffer[0] & 64) - (buffer[0] & 128);
+        let tagnum   = (buffer[0] - lennum) >> 5;
+        let VAL = undefined;
+        let TAG = tags[tagnum];
+
+        let LEN = 0;
+        let TLLEN = 1;
+
+        if(lennum < 24) {
+            VAL = lennum;
+        } else {
+            VAL = buffer.slice(1);
+            if(lennum === 24) { // 1 byte len
+                LEN = readBE81632(VAL.slice(0, 1));
+                VAL = VAL.slice(1);
+                TLLEN += 1;
+            } else if(lennum === 25) { // 2 byte len
+                LEN = readBE81632(VAL.slice(0, 2));
+                VAL = VAL.slice(2);
+                TLLEN += 2;
+            } else if(lennum === 26) { // 4 byte len
+                LEN = readBE81632(VAL.slice(0, 4));
+                VAL = VAL.slice(4);
+                TLLEN += 4;
+            } else if(lennum === 26) { // 8 byte len
+                throw new Error('UNABLE TO READ 8 BYTE LENGTHS')
+            } else if(lennum === 31) { // indefinite length
+                VAL = VAL;
+                LEN = Infinity;
+            } else {    
+                throw new Error(`Found a length of ${lennum}. Length values 28-30(0x1C-0x1E) are reserved!`)
+            }
+
+            if(LEN !== Infinity)
+                VAL = VAL.slice(0, LEN);
+        }
+
+        let TLVTOTALLEN = TLLEN + LEN;
+        return {TAG, LEN, VAL, TLVTOTALLEN, TLLEN}
+    }
+
+    let bufferToString = (buffer) => {
+        if(typeof Buffer !== 'undefined') //NodeJS decoding
+            return buffer.toString('utf8');
+
+        return new TextDecoder('UTF-8').decode(buffer);
+    }
+
+    let removeTagValue = (num) => {
+        return num - (num & 32) - (num & 64) - (num & 128);
+    }
+
+    let bufferToHex = (buffer) => {
+        return Array.from(buffer).map((num) => {
+            if(num < 16)
+                return '0' + num.toString(16)
+
+            return num.toString(16)
+        }).join('')
+    }
+
+
+    let arrayPairsToMap = (seq) => {
+        let finalMap = {};
+        let isKey  = true;
+        let keyVal = '';
+        for(let member of seq) {
+            if(isKey)
+               keyVal = member;
+            else
+                finalMap[keyVal] = member;
+
+            isKey = !isKey;
+        }
+
+        return finalMap
+    }
+
+    let findLegthOfIndefiniteLengthBuffer = (buffer) => {
+        for(let i = 0; i < buffer.length; i++) {
+            if(buffer[i] === 0xff)
+                return i + 1
+        }
+    }
+
+    let decodeCborStream = (buffer, expectedLength) => {
+        buffer = Array.from(buffer);
+ 
+        let results    = [];
+        let bLength    = 0;
+        let workbuffer = buffer.slice();
+
+        for(let i = 0; i < buffer.length; i++) {
+            let tlv = getTLVForNext(workbuffer);
+
+            let result = undefined;
+            switch(tlv.TAG) {
+                case 'UNSIGNED_INT':
+                    if(tlv.TLLEN === 1)
+                        result = tlv.VAL;
+                    else if(tlv.TLLEN <= 5)
+                        result = readBE81632(workbuffer.slice(1, tlv.TLLEN));
+                    else
+                        throw new Error('Ints over 4 bytes are not supported at this moment!');
+
+                    results.push(result);
+                    i += tlv.TLLEN - 1;
+                break
+                case 'NEGATIVE_INT':
+                    if(tlv.TLLEN === 1)
+                        result = -(1 + tlv.VAL);
+                    else if(tlv.TLLEN <= 5)
+                        result = -(1 + readBE81632(workbuffer.slice(1, tlv.TLLEN)));
+                    else
+                        throw new Error('Negative ints over 4 bytes are not supported at this moment!');
+
+                    results.push(result);
+                    i += tlv.TLLEN - 1;
+                break
+                case 'BYTE_STRING':
+                    if(tlv.LEN === Infinity) {
+                        let length = findLegthOfIndefiniteLengthBuffer(tlv.VAL);
+                        result = tlv.VAL.slice(0, length);
+                        i += length;
+                    } else {
+                        if(tlv.LEN === 0) {
+                            result = workbuffer.slice(1, 1 + tlv.VAL)
+                            i += tlv.VAL;
+                        } else {
+                            result = tlv.VAL;
+                            i += tlv.TLVTOTALLEN - 1;
+                        }
+                    }
+
+                    results.push(result);
+                break
+                case 'TEXT_STRING':
+                    if(tlv.LEN === Infinity) {
+                        let length = findLegthOfIndefiniteLengthBuffer(tlv.VAL);
+                        result = tlv.VAL.slice(0, length);
+                        i += length;
+                    } else {
+                        if(tlv.LEN === 0) {
+                            result = workbuffer.slice(1, 1 + tlv.VAL)
+                            i += tlv.VAL;
+                        } else {
+                            result = tlv.VAL;
+                            i += tlv.TLVTOTALLEN - 1;
+                        }
+                    }
+
+                    results.push(bufferToString(result));
+                break
+                case 'ARRAY':
+                    result = decodeCborStream(workbuffer.slice(1), tlv.VAL)
+                    if(result.length !== tlv.VAL)
+                        throw new Error('SEQ missing elements!');
+
+                    results.push(result)
+                    i += result.byteLength;
+                break
+                case 'MAP':
+                    result = decodeCborStream(workbuffer.slice(1), tlv.VAL * 2)
+                    if(result.length !== tlv.VAL * 2)
+                        throw new Error('MAP is missing keypairs!');
+
+                    results.push(arrayPairsToMap(result))
+                    i += result.byteLength;
+                break
+                case 'OTHER_SEM':
+                break
+                case 'FLOAT_AND_NO_CONTENT':
+                    let type = float_and_no_content_semantics[tlv.VAL];
+                    switch(type) {
+                        case "FALSE":
+                            results.push(false);
+                            i += 1;
+                        break
+                        case "TRUE":
+                            results.push(true);
+                            i += 1;
+                        break
+                        case "NULL":
+                            results.push(null);
+                            i += 1;
+                        break
+                        case "UNDEFINED":
+                            results.push(undefined);
+                            i += 1;
+                        break
+                        // case "SIMPLE_CONT"
+                        // break
+                        // case "FLOAT_16"
+                        // break
+                        // case "FLOAT_32"
+                        // break
+                        // case "FLOAT_64"
+                        // break
+                        // case "INFINITY_BREAK"
+                        // break
+                        default:
+                            if(!type)
+                                throw new Error(`VALUE ${tlv.VAL} IS UNASSIGNED`);
+
+                            throw new Error(`${type} IS NOT CURRENTLY IMPLEMENTED!`);
+                        break
+                    }
+                break
+            }
+
+            workbuffer = buffer.slice(i + 1)
+            bLength    = i + 1; 
+
+            if(expectedLength && results.length === expectedLength)
+                break
+        }
+
+        results.byteLength = bLength;
+        return results
+    }
+
+    let methods = {
+        'decode': decodeCborStream,
+        'decodeOnlyFirst': (buffer) => {
+            return decodeCborStream(buffer, 1)
+        },
+        'encode': () => {
+            throw new Error('NOT IMPLEMENTED YET')
+        }
+    }
+
+    /**
+     * Exporting and stuff
+     */
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = methods;
+
+    } else {
+        if (typeof define === 'function' && define.amd) {
+            define([], function() {
+                return methods
+            });
+        } else {
+            window.vanillaCBOR = methods;
+        }
+    }
+})()
