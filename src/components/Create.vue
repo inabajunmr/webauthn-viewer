ae621959b46201dad7a44c63cd6071b1b2722a62
jun-inaba <jun.inaba@dena.com>
2023-06-26 16:53:31 +0900

    wip

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -674,7 +674,7 @@ export default {
   methods: {
     async create() {
 
-      const op = await fetch('https://google.com', {
+      const op = await fetch('/', {
         credentials: 'same-origin',
       }).then(response => {
         return response
