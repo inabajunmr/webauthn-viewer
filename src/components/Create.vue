0c92704eb5c27f675b58c209cbce00982bdd559f
jun-inaba <jun.inaba@dena.com>
2023-06-27 12:29:49 +0900

    Revert "wip"
    This reverts commit 5f145f5d1f81ab6f02e5bd5c87442ce312d74f23.

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -689,7 +689,7 @@ export default {
 
       // call webauthn api
       console.log("Create Request", this.buildCreateRequest);
-      window.navigator.credentials
+      navigator.credentials
         .create(this.buildCreateRequest)
         .then(res => {
           console.log("Create Response", res);
