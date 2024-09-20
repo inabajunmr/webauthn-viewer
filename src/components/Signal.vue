4ca447081a9c0da088f27657339bee43f3d2203b
inabajunmr <inabajun.for.regi@gmail.com>
2024-09-21 02:32:22 +0900

    signals

--- src/components/Signal.vue
+++ src/components/Signal.vue
@@ -2,7 +2,9 @@
   <div class="container is-size-7">
     <div class="columns">
       <div class="column is-one-third">
-        <h3 class="title">signalCurrentUserDetails</h3>
+
+
+        <h3 class="title">signalUnknownCredential</h3>
         <h4 class="title">Request</h4>
         <div class="field">
           <label class="label is-small">rpid</label>
@@ -11,47 +13,31 @@
               class="input is-small"
               type="text"
               placeholder="rpId"
-              v-model="signalCurrentUserDetailsRpId"
+              v-model="signalUnknownCredentialRpId"
             />
           </div>
-          <label class="label is-small">userId</label>
+          <label class="label is-small">credentialId</label>
           <div class="control">
             <input
               class="input is-small"
               type="text"
-              placeholder="userId"
-              v-model="signalCurrentUserDetailsUserId"
+              placeholder="id"
+              v-model="signalUnknownCredentialCredentialId"
             />
           </div>
-          <label class="label is-small">name</label>
-          <div class="control">
-            <input
-              class="input is-small"
-              type="text"
-              placeholder="name"
-              v-model="signalCurrentUserDetailsName"
-            />
-          </div>
-          <label class="label is-small">displayName</label>
-          <div class="control">
-            <input
-              class="input is-small"
-              type="text"
-              placeholder="displayName"
-              v-model="signalCurrentUserDetailsDisplayName"
-            />
-          </div>          
         </div>
 
         <input
           type="button"
-          value="signalCurrentUserDetails()"
+          value="signalUnknownCredential()"
           class="button is-primary is-large"
-          @click="signalCurrentUserDetails()"
+          @click="signalUnknownCredential()"
         />
+        
 
         <br>
         <br>
+
 
         <h3 class="title">signalAllAcceptedCredentials</h3>
         <h4 class="title">Request</h4>
@@ -105,8 +91,81 @@
           class="button is-primary is-large"
           @click="signalAllAcceptedCredentials()"
         />
+
+        <br>
+        <br>
+
+        <h3 class="title">signalCurrentUserDetails</h3>
+        <h4 class="title">Request</h4>
+        <div class="field">
+          <label class="label is-small">rpid</label>
+          <div class="control">
+            <input
+              class="input is-small"
+              type="text"
+              placeholder="rpId"
+              v-model="signalCurrentUserDetailsRpId"
+            />
+          </div>
+          <label class="label is-small">userId</label>
+          <div class="control">
+            <input
+              class="input is-small"
+              type="text"
+              placeholder="userId"
+              v-model="signalCurrentUserDetailsUserId"
+            />
+          </div>
+          <label class="label is-small">name</label>
+          <div class="control">
+            <input
+              class="input is-small"
+              type="text"
+              placeholder="name"
+              v-model="signalCurrentUserDetailsName"
+            />
+          </div>
+          <label class="label is-small">displayName</label>
+          <div class="control">
+            <input
+              class="input is-small"
+              type="text"
+              placeholder="displayName"
+              v-model="signalCurrentUserDetailsDisplayName"
+            />
+          </div>          
+        </div>
+
+        <input
+          type="button"
+          value="signalCurrentUserDetails()"
+          class="button is-primary is-large"
+          @click="signalCurrentUserDetails()"
+        />
         
       </div>
+      <div class="column">
+        <h3 class="title">Response</h3>
+        <table
+          class="table is-responsive"
+          style="table-layout: fixed; width: 100%"
+        >
+          <tbody>
+            <tr>
+              <th>Error</th>
+              <td style="word-wrap: break-word" class="has-text-danger">
+                {{ errorType }}
+              </td>
+            </tr>
+            <tr>
+              <th>Error message</th>
+              <td style="word-wrap: break-word" class="has-text-danger">
+                {{ errorMessage }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>      
     </div>
   </div>
 </template>
@@ -124,6 +183,8 @@ export default {
       signalAllAcceptedCredentialsRpId: window.location.hostname,
       signalAllAcceptedCredentialsUserId: this.generateRandomUserId(),
       signalAllAcceptedCredentialsAllAcceptedCredentals: [{id:"", r:Math.random()}],
+      signalUnknownCredentialRpId: window.location.hostname,
+      signalUnknownCredentialCredentialId: ""
     };
   },
   computed: {
@@ -141,7 +202,48 @@ export default {
         .toString("hex");
       return this.reqUserId;
     },
+    signalUnknownCredential() {
+      if(!window.PublicKeyCredential.signalUnknownCredential) {
+        this.errorMessage = "PublicKeyCredential.signalUnknownCredential is undefined";
+        return;
+      }
+
+      window.PublicKeyCredential.signalUnknownCredential({
+        rpId: this.signalUnknownCredentialRpId,
+        credentialId: this.signalUnknownCredentialCredentialId
+      }).catch(err => {
+          console.log("signalUnknownCredential Error", err);
+          this.errorType = err.name;
+          this.errorMessage = err.message;
+        });
+    },
+    signalAllAcceptedCredentials() {
+      if(!window.PublicKeyCredential.signalAllAcceptedCredentials) {
+        this.errorMessage = "PublicKeyCredential.signalAllAcceptedCredentials is undefined";
+        return;
+      }
+
+      window.PublicKeyCredential.signalAllAcceptedCredentials({
+        rpId: this.signalAllAcceptedCredentialsRpId,
+        userId: Buffer.from(this.signalAllAcceptedCredentialsUserId, "hex").toString('base64').replace(/\+/g, '-')
+                .replace(/\//g, '_')
+                .replace(/=/g, ''),
+        allAcceptedCredentialIds:this.signalAllAcceptedCredentialsAllAcceptedCredentals.map(obj => obj.id)
+      }).catch(err => {
+          console.log("signalAllAcceptedCredentials Error", err);
+          this.errorType = err.name;
+          this.errorMessage = err.message;
+        });
+    },
+    addAcceptedCredentals() {
+      this.signalAllAcceptedCredentialsAllAcceptedCredentals.push({id:"", r:Math.random()});
+    },
     signalCurrentUserDetails() {
+      if(!window.PublicKeyCredential.signalCurrentUserDetails) {
+        this.errorMessage = "PublicKeyCredential.signalCurrentUserDetails is undefined";
+        return;
+      }
+
       window.PublicKeyCredential.signalCurrentUserDetails({
         rpId: this.signalCurrentUserDetailsRpId,
         userId: Buffer.from(this.signalCurrentUserDetailsUserId, "hex").toString('base64').replace(/\+/g, '-')
@@ -149,20 +251,12 @@ export default {
                 .replace(/=/g, ''),
         name: this.signalCurrentUserDetailsName,
         displayName: this.signalCurrentUserDetailsDisplayName
-      });
+      }).catch(err => {
+          console.log("signalCurrentUserDetails Error", err);
+          this.errorType = err.name;
+          this.errorMessage = err.message;
+        });
     },
-    signalAllAcceptedCredentials() {
-      window.PublicKeyCredential.signalAllAcceptedCredentials({
-        rpId: this.signalAllAcceptedCredentialsRpId,
-        userId: Buffer.from(this.signalAllAcceptedCredentialsUserId, "hex").toString('base64').replace(/\+/g, '-')
-                .replace(/\//g, '_')
-                .replace(/=/g, ''),
-        allAcceptedCredentialIds:this.signalAllAcceptedCredentialsAllAcceptedCredentals.map(obj => obj.id)
-      });
-    },
-    addAcceptedCredentals() {
-      this.signalAllAcceptedCredentialsAllAcceptedCredentals.push({id:"", r:Math.random()});
-    }
   }
 };
 </script>
